5f42b89ac787ba5c9ab151cc1de4cbd3b4922564
Szöllősi Bálint (SZF_2023_2025) <szollosi.balint@diak.szamalk-szalezi.hu>
2023-11-08 13:58:08 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import math
+import random
+
+
+def feladat1():
+    i:int=0
+    while i<5:
+        szam: int = math.floor(random.random()*90-1)
+        print(szam,end=", ")
+        i+=1
+feladat1()
+def feladat2():
+    szam: int = math.floor(random.random()*90-20)
+    pass
